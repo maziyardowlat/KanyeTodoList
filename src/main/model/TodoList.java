53853e650ae665dc04136c0cd00568e8b0e3d6ec
Maziyar Dowlatabadibazaz <maziyardowlat@gmail.com>
2021-07-20 14:30:43 -0600

    Got started on the console log, but it doesnt do much now with the proper commands.

--- src/main/model/TodoList.java
+++ src/main/model/TodoList.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TodoList {
-    private List<Task> tasks;
+    private ArrayList<Task> tasks;
     private Task task1;
 
+    //Constructor, makes a new TodoList
     public TodoList() {
         tasks = new ArrayList<>();
     }
 
+    //EFFECTS: returns the todolist items
     public List<Task> getTasks() {
         return tasks;
     }
 
+    //MODIFIES: this
+    //EFFECTS: adds a new task to the todolist
     public void addTask(Task t) {
         tasks.add(t);
     }
--- src/main/ui/TodoListApp.java
+++ src/main/ui/TodoListApp.java
@@ -1,5 +1,74 @@
 package ui;
 
+import model.Task;
+import model.TodoList;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TodoListApp {
+    private Task task1;
+    private TodoList list1;
+    private Scanner input;
+
+    // EFFECTS: runs the teller application
+    public TodoListApp() {
+        runList();
+    }
+
+    private void runList() {
+        boolean keepGoing = true;
+        String command = null;
+
+        init();
+
+        while (keepGoing) {
+            displayTask();
+            command = input.next();
+            command = command.toLowerCase();
+
+            if (command.equals("q")) {
+                keepGoing = false;
+            } else {
+                processCommand(command);
+            }
+        }
+
+        System.out.println("Good Job Today!");
+
+    }
+
+    private void init() {
+        input = new Scanner(System.in);
+        task1 = new Task("asdf", "asdf", "asdf", "asdf", "asdf");
+
+    }
+
+    private void displayTask() {
+        System.out.println("\nSelect from:");
+        System.out.println("\tn -> Add a new Task");
+        System.out.println("\tv -> View Current Tasks");
+
+    }
+
+    private void processCommand(String command) {
+        if (command.equals("n")) {
+            doNewList();
+        } else if (command.equals("v")) {
+            doShowTask();
+        } else {
+            System.out.println("What are you doing Bubsy. Choose another choice");
+        }
+    }
+
+    private void doNewList(){
+
+    }
+
+    private void doShowTask(){
+
+    }
+
+
 
 }
